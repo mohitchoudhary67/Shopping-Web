f6a5328b07c8ffefed9e5b383795108958ef6f17
mohit <mohitchodhary96@gmail.com>
2024-06-23 17:56:07 +0530

    Ecom

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('Welcome to my javascript code!');
+
+let burger = document.querySelector('.burger');
+let navbar = document.querySelector('#navbar');
+mohit = document.querySelector('.mohit');
+let close = document.getElementById('#close');
+
+
+
+
+burger.addEventListener('click', () => {
+    navbar.classList.toggle('mohit');
+})
+
+
+
+
+
